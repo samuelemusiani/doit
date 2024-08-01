c0e1641a35078fb9bc81c54a011a8596b0dd1401
Samuele Musiani <samu@teapot.ovh>
2024-08-02 00:20:30 +0200

    UserResponse

--- cmd/doit/types.go
+++ cmd/doit/types.go
@@ -17,3 +17,21 @@ type User struct {
 	Active   bool
 	Password string
 }
+
+type UserResponse struct {
+	ID       int64
+	Username string
+	Email    string
+	Name     string
+	Surname  string
+}
+
+func UserToResponse(u *User) *UserResponse {
+	return &UserResponse{
+		ID:       u.ID,
+		Username: u.Username,
+		Email:    u.Email,
+		Name:     u.Name,
+		Surname:  u.Surname,
+	}
+}
