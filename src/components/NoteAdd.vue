327f60f6665a28884823d9b5e23cdf57f142b383
Samuele Musiani <samu@teapot.ovh>
2024-08-16 00:39:17 +0200

    style

--- src/components/NoteAdd.vue
+++ src/components/NoteAdd.vue
@@ -24,6 +24,10 @@ function close() {
 }
 
 function addNote() {
+  if (_title.value === '') {
+    return
+  }
+
   let n: Todo = { ID: 0, Title: _title.value, Description: _description.value }
 
   $emits('addNote', n)
@@ -31,18 +35,27 @@ function addNote() {
 </script>
 
 <template>
-  <div class="fixed w-[20rem] rounded border bg-white" :style="style" ref="__top_div">
-    <header class="flex justify-between bg-gray-200">
+  <div class="fixed w-[20rem] rounded border border-black bg-white" :style="style" ref="__top_div">
+    <header class="flex justify-between rounded bg-gray-200">
       <h1 class="flex-auto p-5 text-center font-bold">Add a Note</h1>
     </header>
 
     <body class="">
       <form class="flex flex-col p-5" @submit.prevent="">
         <label>Title</label>
-        <input type="text" class="rounded border p-2" required v-model="_title" />
+        <input
+          type="text"
+          class="rounded border p-2 focus:outline-none"
+          required
+          v-model="_title"
+        />
 
         <label class="mt-5">Description</label>
-        <textarea type="text" class="rounded border p-2" v-model="_description"></textarea>
+        <textarea
+          type="text"
+          class="rounded border p-2 focus:outline-none"
+          v-model="_description"
+        ></textarea>
 
         <div class="mt-5 flex justify-around">
           <button @click="close()" class="rounded bg-red-200 p-2 hover:bg-red-400">Close</button>
