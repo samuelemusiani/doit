c5d547d77a85f8cc52934192daae0420ccb45fd7
Samuele Musiani <samu@teapot.ovh>
2024-08-18 00:11:18 +0200

    better routes navigation

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -9,15 +9,24 @@ function logout() {
     method: 'DELETE',
     credentials: 'include'
   }).catch((error) => console.log(error))
-
-  $router.push('login')
 }
 </script>
 
 <template>
   <div class="flex justify-between bg-gray-800">
-    <h1 class="p-2 text-4xl font-bold text-gray-200">DOIT</h1>
-    <button class="p-2 text-gray-200 hover:bg-gray-700" @click="logout()">Logout</button>
+    <RouterLink :to="{ name: 'home' }" class="grid p-2 text-gray-200 hover:bg-gray-700">
+      <h1 class="text-4xl font-bold text-gray-200">DOIT</h1>
+    </RouterLink>
+    <RouterLink :to="{ name: 'profile' }" class="grid p-2 text-gray-200 hover:bg-gray-700">
+      <span class="place-self-center"> Profile </span>
+    </RouterLink>
+    <RouterLink
+      :to="{ name: 'login' }"
+      class="grid p-2 text-gray-200 hover:bg-gray-700"
+      @click="logout()"
+    >
+      <span class="place-self-center"> Logout </span>
+    </RouterLink>
   </div>
 </template>
 
