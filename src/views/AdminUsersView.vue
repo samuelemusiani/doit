5106a21472f8151ae230c815af6ee00bbb2508bd
Samuele Musiani <samu@teapot.ovh>
2024-08-18 20:14:06 +0200

    style

--- src/views/AdminUsersView.vue
+++ src/views/AdminUsersView.vue
@@ -11,13 +11,16 @@ onMounted(() => {
 </script>
 
 <template>
-  <div>
+  <div class="ml-5 mt-5">
     <button class="rounded border p-2 hover:bg-gray-100" @click="$router.go(-1)">Go back</button>
   </div>
   <div class="flex justify-center">
-    <div>
-      <h1 class="m-5 text-center font-bold">USERS VIEW</h1>
-      <div class="flex flex-col gap-2">
+    <div class="w-1/3 rounded border p-5">
+      <div class="mt-2 flex justify-between">
+        <h1 class="text-center font-bold">Users list</h1>
+        <div>Total: {{ _users.length }}</div>
+      </div>
+      <div class="mt-5 flex flex-col gap-2">
         <template v-for="user in _users" :key="user.ID">
           <RouterLink :to="{ name: 'user_details', params: { id: user.ID } }">
             <div class="rounded border p-2 hover:bg-gray-100">
--- src/views/AdminView.vue
+++ src/views/AdminView.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    <h1 class="m-5 text-2xl font-bold">Admin page</h1>
-  </div>
-  <div>
-    <RouterLink :to="{ name: 'users' }" class="rounded border p-2 hover:bg-gray-100">
-      Users
-    </RouterLink>
+  <div class="flex justify-center">
+    <div>
+      <div>
+        <h1 class="m-5 text-2xl font-bold">Admin page</h1>
+      </div>
+      <div class="w-full">
+        <RouterLink :to="{ name: 'users' }">
+          <div class="w-full rounded border p-2 hover:bg-gray-100">Users</div>
+        </RouterLink>
+      </div>
+    </div>
   </div>
 </template>
 
